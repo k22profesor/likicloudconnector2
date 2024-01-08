a1e00c9beab82109547b264f6e27af55ae52231a
k22profesor1 <155194711+k22profesor@users.noreply.github.com>
2024-01-09 00:57:47 +0100

    pełen program

--- main.ts
+++ main.ts
@@ -1,7 +1,5 @@
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
     RxData = serial.readLine().split("=")
-    basic.showString("" + (RxData[1]))
-    basic.pause(5000)
 })
 let temp = 0
 let RxData: string[] = []
@@ -10,17 +8,19 @@ SerialPin.P12,
 SerialPin.P16,
 BaudRate.BaudRate115200
 )
+serial.setRxBufferSize(128)
 basic.forever(function () {
     temp = input.temperature()
     serial.writeLine("DATA(A,Temperatura):" + convertToText(temp))
-    basic.pause(2000)
-})
-basic.forever(function () {
-    basic.pause(200)
+    basic.pause(1000)
     serial.writeLine("REQUEST(Sheet3,F414)")
-    basic.pause(200)
-    serial.writeLine("URL(https://docs.google.com/spreadsheets/d/1zX8XiftKqrxGiFI5nmw1hhmamDaujJcoGMLj1WMFL2E/edit?usp=sharing)")
+    basic.pause(1000)
+    basic.showString("" + (RxData[1]))
     basic.pause(5000)
+    serial.writeLine("URL(https://docs.google.com/spreadsheets/d/1zX8XiftKqrxGiFI5nmw1hhmamDaujJcoGMLj1WMFL2E/edit?usp=sharing)")
+    basic.pause(1000)
     serial.writeLine("WHAT(TIME)")
+    basic.pause(1000)
+    basic.showString("" + (RxData[1]))
     basic.pause(5000)
 })
